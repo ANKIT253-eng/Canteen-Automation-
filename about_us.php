f8ffe63cadef26b3d10d50b106fadc88a2d84ea5
ankit <ankit88mth@gmail.com>
2022-05-29 16:17:08 +0530

    first commit

---
+++ about_us.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>About US</title>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Karma">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    
+    <style type="text/css">
+       body{
+       background-image: url("images/bg3.jpg");
+       background-size: 100%;
+       background-repeat: no-repeat;
+
+    }
+    
+  </style>
+</head>
+<body>
+  <h1 align="center" style="color: black">THE TEAM</h1>
+
+  
+
+
+  <div class="w3-row-padding w3-padding-16 w3-center" style="color:white">
+    <div class="w3-quarter">
+      <img class="img-rounded " src="images/ankit.jpg" alt="Ankit kumar" style="width: 350px;height: 350px">
+      <h3>Ankit Kumar</h3>
+      <p> Developer</p>
+    </div>
+
+<p style="color:white">&copy; ITS Engineering college</p>
+  
+
+</body>
+</html>
+ 
+
